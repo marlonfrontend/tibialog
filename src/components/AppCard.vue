2804aba93a0e0f4362ee7df3040be81df11268fe
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-06-07 09:35:41 -0300

    feat: created card and file input component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const props = defineProps({
+  title: String
+})
+
+defineExpose(props)
+</script>
+<template>
+  <div
+    class="border py-12 border-opacity-5 border-white flex flex-col w-full items-center justify-center"
+  >
+    <div class="text-xl text-white mb-5">{{ title }}</div>
+    <slot />
+  </div>
+</template>
---
+++ src/components/AppFileInput.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const emit = defineEmits(['change'])
+
+const props = defineProps({
+  label: String
+})
+
+const emitFileChange = ($event: any) => {
+  emit('change', $event.target.files[0])
+}
+
+defineExpose(props)
+</script>
+<template>
+  <label
+    class="border text-white py-3 transition-all duration-200 border-opacity-10 hover:bg-white hover:bg-opacity-5 border-white px-5 rounded-lg cursor-pointer hover:border-opacity-5"
+  >
+    <span class="text-sm leading-normal">{{ label }}</span>
+    <input type="file" accept="text/plain" @change="emitFileChange" class="hidden" />
+  </label>
+</template>
